f4d651a3770eb331b40f220dcf49eea66d03723f
sylaaang <mehdi.hachem@yutopia.io>
2025-05-13 13:26:58 +0200

    add allowed_hots on .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8080',
-    'aurasync-fit-together-friends.vercel.app',
+    'https://fit-together-friends.vercel.app',
 ]
 
 ROOT_URLCONF = 'backend.urls'
